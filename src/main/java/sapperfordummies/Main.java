74556349ea23713882b667533880a7571673d7e2
Vadim Rodionov <tuukvadim@live.com>
2022-01-01 19:56:32 +0300

    Небольшая доработка для тех, кто захочет запустить и поигать)

--- src/main/java/sapperfordummies/Main.java
+++ src/main/java/sapperfordummies/Main.java
@@ -6,34 +6,33 @@ package sapperfordummies;
  *
  * @author Vadim
  */
-public class SapperForDummies {
+public class Main {
 
     public static void main(String[] args) {
         SettingsAndGame play = new SettingsAndGame();
         String str = new String();
-        System.out.println("\tGreetings\nPlease enter:"
+        System.out.println("\tGreetings"
+              + "\nPlease enter:"
               + "\n 'rules' - to read rules"
               + "\n 'settings' - to access to rules and set size field or mines"
               + "\n 'play' - to play the game"
               + "\n 'qqq' - to exit from game");
         str = play.strEntered();
         do {
-            if (str.equals("rules")) {
-                play.rules();
-                str = "";
-            }
-            if (str.equals("settings")) {
-                play.callSettings(str);
-                str = "";
-            }
-
-            if (str.equals("play")) {
-                play.startGame();
-            }
-            if (!str.equals("settings") && !str.equals("play")
-                    && !str.equals("qqq") && !str.equals("rules")) {
-                System.out.print("Please, Re-enter: ");
-                str = play.strEntered();
+            switch (str) {
+                case "rules" -> {
+                    play.rules();
+                    str = "";
+                }
+                case "settings" -> {
+                    play.callSettings(str);
+                    str = "";
+                }
+                case "play" -> play.startGame();
+                default -> {
+                    System.out.print("Please, Re-enter: ");
+                    str = play.strEntered();
+                }
             }
         } while (!str.equals("qqq"));
     }
